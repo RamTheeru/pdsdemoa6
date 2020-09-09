4b9053ba3f3822633b6a7d6e278a10756b1d4db3
RamTheeru <theertha808@gmail.com>
2020-09-09 20:58:41 +0530

    5 files modified

--- src/app/loginhome/loginhome.component.ts
+++ src/app/loginhome/loginhome.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loginhome.component.css']
 })
 export class LoginhomeComponent implements OnInit {
+  shoesidenav : Boolean = false;
  //shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
   constructor() { }
 
   ngOnInit() {
+  }
+  Onbtnclick(){
+    this.shoesidenav = !this.shoesidenav;
   }
 
 }
