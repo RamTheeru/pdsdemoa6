c3b863832599fc9ac1e35554dc16a4532fbbfa3f
RamTheeru <theertha808@gmail.com>
2020-09-12 18:00:16 +0530

    4 files modified

--- src/app/loginhome/create-employee/create-employee.component.ts
+++ src/app/loginhome/create-employee/create-employee.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup,FormBuilder,FormControl,FormArray,Validators} from '@angular/forms';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
-  constructor() { }
+empForm2 : FormGroup;
+  constructor(private _fb:FormBuilder) { 
+    this.initForm();
+  }
 
   ngOnInit() {
+  }
+  initForm(){
+
+  this.empForm2 = this._fb.group({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    middleName: new FormControl(),
+        });
+
+  }
+  onSubmit(){
+
   }
 
 }
