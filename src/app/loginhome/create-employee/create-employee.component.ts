b5daa2ae1308ccc9823ccd9f42f364731c6e05b3
RamTheeru <theertha808@gmail.com>
2020-09-12 20:08:48 +0530

    2 files modified

--- src/app/loginhome/create-employee/create-employee.component.ts
+++ src/app/loginhome/create-employee/create-employee.component.ts
@@ -7,6 +7,8 @@ import {FormGroup,FormBuilder,FormControl,FormArray,Validators} from '@angular/f
 })
 export class CreateEmployeeComponent implements OnInit {
 empForm2 : FormGroup;
+  maritals = ['married','unmarried'];
+   empTypes = ['permanent','contract'];
   constructor(private _fb:FormBuilder) { 
     this.initForm();
   }
@@ -23,6 +25,50 @@ empForm2 : FormGroup;
 
   }
   onSubmit(){
+
+  }
+
+   focusOutFunction(field,event:any):void{
+
+      const errorTitle : string = 'INVALID INPUT!!!';
+       var txt = event.target.value;
+    if(field=='fname'){
+      var f = 'First Name';
+      this.showrequiredMessage(f,txt,errorTitle);
+         
+    }
+    if(field == 'phone'){
+        var f = 'Employee Contact Number';
+        this.showrequiredMessage(f,txt,errorTitle);
+    }
+
+  }
+  showrequiredMessage(field,txt,title){
+        
+          var test = false;
+          if(txt == '' || txt==null){
+          var msg = field+' '+' field required!!';
+            this._swServ.showErrorMessage(title,msg);
+          }
+          else if(field == 'Employee Contact Number')
+          {
+            var msg = field+' '+' contains Only Numbers!!';
+             test = this.ValidateNumbers(txt);
+             if(!test){
+               this._swServ.showErrorMessage(title,msg);
+               
+             }
+
+
+              
+          }
+  }
+
+  ValidateNumbers(txt:string) : boolean{
+      var val  = false;
+       var regexp = new RegExp('^[0-9]+$');
+       val = regexp.test(txt);
+      return val;
 
   }
 
