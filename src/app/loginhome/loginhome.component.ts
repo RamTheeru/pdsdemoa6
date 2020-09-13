30600762aedb9b122807e99e6bcee262ace2f8b0
RamTheeru <theertha808@gmail.com>
2020-09-13 21:38:57 +0530

    4 files modified

--- src/app/loginhome/loginhome.component.ts
+++ src/app/loginhome/loginhome.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,ViewChild, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 @Component({
   selector: 'app-loginhome',
   templateUrl: './loginhome.component.html',
@@ -9,6 +9,23 @@ export class LoginhomeComponent implements OnInit {
   shoesidenav : Boolean = false;
   shownotify : Boolean = true;
   actTab:Boolean=false;
+    @ViewChild('sidenav') sidenav: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
  //shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
   constructor() { }
 
