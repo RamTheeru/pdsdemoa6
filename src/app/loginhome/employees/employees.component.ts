c9f5be621b0b3b8b699c556a04d8af3b89f2606d
RamTheeru <theertha808@gmail.com>
2020-10-24 15:21:52 +0530

    1 file modified

--- src/app/loginhome/employees/employees.component.ts
+++ src/app/loginhome/employees/employees.component.ts
@@ -10,23 +10,67 @@ import { SweetService } from "../../sweet.service";
 })
 export class EmployeesComponent implements OnInit {
   employees: Employee[];
+  e: Employee;
   apiResult: APIResult;
   constructor(private api: PdsApiService, private swServ: SweetService) {}
+  getstaticEmployees() {
+    const emp: Employee = new Employee();
+    const errorTitle: string = "INVALID INPUT!!!";
 
+    emp.FirstName = "Ram";
+    emp.LastName = "k";
+    emp.MiddleName = "";
+    emp.Phone = "62463732424";
+
+    emp.Age = 29;
+    emp.BloodGroup = "O+";
+    emp.Gender = "m";
+    emp.Marital = "married";
+
+    emp.Address1 = "D.NO2-65";
+    emp.Adress2 = "pragathi nagar";
+    emp.Place = "atp";
+    emp.State = "AP";
+    // emp.PostalCode = ;
+    emp.AAdharNumber = "236264657";
+    emp.PAN = "Aj24u23985";
+    emp.Guard_FullName = "Ramdas";
+
+    emp.Guard_Phone = "5353463473";
+    emp.DOB = "09-09-1990";
+    emp.DOJ = "09-09-2020";
+
+    emp.Designation = "Office Assisstnat";
+    emp.StationCode = "gtkl";
+    emp.LocationName = "Guntakal";
+    emp.DLLRStatus = "NO";
+    emp.VehicleNumber = "";
+    emp.DLLRNumber = "";
+    emp.BankAccountNumber = "35643637537";
+    emp.BankName = "Kotak";
+    emp.IFSCCode = "KTKB43523";
+    emp.BranchName = "Madhapur";
+
+    return emp;
+  }
   ngOnInit() {
-    this.api.getEmployees().subscribe(data => {
-      console.log(data);
-      this.apiResult = data;
-      console.log("" + this.apiResult.Status);
-      this.apiResult.Status = data.status;
-      this.apiResult.Message = data.message;
+    this.e = this.getstaticEmployees();
+    this.employees.push(this.e);
+    this.e = this.getstaticEmployees();
+    this.employees.push(this.e);
+    // this.api.getEmployees().subscribe(data => {
+    //   console.log(data);
+    //   this.apiResult = data;
+    //   console.log("" + this.apiResult.Status);
+    //   this.apiResult.Status = data.status;
+    //   this.apiResult.Message = data.message;
 
-      if (this.apiResult.Status) {
-        this.employees = this.apiResult.employees;
-        console.log(this.employees);
-      } else {
-        this.swServ.showErrorMessage("Failure", this.apiResult.Message);
-      }
-    });
+    //   if (this.apiResult.Status) {
+    //     this.employees = this.apiResult.employees;
+    //     console.log(this.employees);
+    //   } else {
+    //     this.swServ.showErrorMessage("Failure", this.apiResult.Message);
+    //   }
+    // });
   }
 }
