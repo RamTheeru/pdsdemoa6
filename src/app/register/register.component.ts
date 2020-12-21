d29400e67d627d29cc45167a4e1510df0c4b9ec3
RamTheeru <theertha808@gmail.com>
2020-12-21 20:51:22 +0530

    1 file modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -201,14 +201,14 @@ export class RegisterComponent implements OnInit {
     //   .filter(v => v !== null);
     //   console.log('checkboxes')
     //   console.log(selectempTypes);
-    emp.EmpCode = "";
+    emp.EmpCode = "-";
     emp.FirstName = this.empForm.value["firstName"];
     emp.LastName = this.empForm.value["lastName"];
     emp.MiddleName = this.empForm.value["middleName"];
     emp.Phone = this.empForm.value["phone"];
     emp.DOB = db;
     emp.DOJ = dj;
-
+    emp.EmpCode = emp.Phone;
     //emp.Marital = marit;
     // emp.Day = this.empForm.value["day"];
     // emp.Month = this.empForm.value["month"];
