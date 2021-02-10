2deb780a52aee86b10e10739704811837ae9b737
RamTheeru <theertha808@gmail.com>
2021-02-10 14:40:34 +0530

    2 files modified

--- src/app/loginhome/downloadinvoice/downloadinvoice.component.ts
+++ src/app/loginhome/downloadinvoice/downloadinvoice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Employee } from "../../models/employee";
 import { APIResult } from "../../models/apiresult";
 import { PdsApiService } from "../../pds-api.service";
@@ -7,21 +7,42 @@ import { SweetService } from "../../sweet.service";
 import { UserType } from "../../models/usertype";
 import { PDFInput } from "../../models/pdfinput";
 import { ViewService } from "../../view.service";
+import * as r from "rxjs";
 import swal from "sweetalert2";
 @Component({
   selector: "app-downloadinvoice",
   templateUrl: "./downloadinvoice.component.html",
   styleUrls: ["./downloadinvoice.component.css"]
 })
-export class DownloadinvoiceComponent implements OnInit {
+export class DownloadinvoiceComponent implements OnInit, OnDestroy {
   showPath: boolean = false;
   usrToken: string = "";
+  userInfo: UserType;
+  private subsc: r.Subscription;
+  private subsc2: r.Subscription;
   path: string = "";
+  pageCount: number = 1;
+  pages = [];
   stationId: number = 0;
+  totalCount: number = 0;
   load: boolean = false;
   currentmonth: number = 0;
   apiInput: ApiInput;
   employees: Employee[] = [];
+  months = [
+    { id: 1, name: "January" },
+    { id: 2, name: "Febrauary" },
+    { id: 3, name: "March" },
+    { id: 4, name: "April" },
+    { id: 5, name: "May" },
+    { id: 6, name: "June" },
+    { id: 7, name: "July" },
+    { id: 8, name: "August" },
+    { id: 9, name: "September" },
+    { id: 10, name: "October" },
+    { id: 11, name: "November" },
+    { id: 12, name: "December" }
+  ];
   constructor(
     private swServ: SweetService,
     private api: PdsApiService,
@@ -41,11 +62,29 @@ export class DownloadinvoiceComponent implements OnInit {
       this.usrToken == null
     ) {
       this.handleUnauthorizedrequest();
+    } else if (this.stationId == 0) {
+      this.swServ.showErrorMessage(
+        "Something Went Wrong!!",
+        "Unable to get Station, Please try again!!"
+      );
     } else {
-      this.apiInput = new ApiInput();
-      this.apiInput.stationId = Number(this.stationId);
-      this.apiInput.currentmonth = this.currentmonth;
-      this.getemployees(this.apiInput);
+      swal({
+        title: "Are you sure?",
+        text:
+          "Do you want to Download Invoice for all CDAs under this station?",
+        type: "warning",
+        showConfirmButton: true,
+        showCancelButton: true
+      }).then(willDelete => {
+        if (willDelete.value) {
+          this.apiInput = new ApiInput();
+          this.apiInput.stationId = Number(this.stationId);
+          this.apiInput.currentmonth = this.currentmonth;
+          this.getemployees(this.apiInput);
+        } else {
+          this.swServ.showErrorMessage("Canelled", "");
+        }
+      });
     }
   }
   handleUnauthorizedrequest() {
@@ -55,21 +94,6 @@ export class DownloadinvoiceComponent implements OnInit {
     );
   }
   shwPath() {
-    swal({
-      title: "Are you sure?",
-      text: "Do you want to Download Invoice for all CDAs under this station?",
-      type: "warning",
-      showConfirmButton: true,
-      showCancelButton: true
-    }).then(willDelete => {
-      if (willDelete.value) {
-        // console.log(deliverylist);
-        // this.updateDeilveryDetails(deliverylist, this.usrToken);
-        // this.api.approveUser(e.RegisterId, status);
-      } else {
-        this.swServ.showErrorMessage("Canelled", "");
-      }
-    });
     // if (!this.showPath) {
     //   this.showPath = true;
     // }
@@ -87,7 +111,10 @@ export class DownloadinvoiceComponent implements OnInit {
           this.pageCount = data.queryPages;
           this.totalCount = data.queryTotalCount;
           this.pages = this.api.transform(this.pageCount);
-          console.log(data);
+          if (this.totalCount > 0) {
+          } else {
+            this.swServ.showMessage("Warning!!!", message);
+          }
         } else {
           this.swServ.showErrorMessage("Failure!!!", message);
         }
@@ -100,6 +127,23 @@ export class DownloadinvoiceComponent implements OnInit {
     this.subsc2 = this.vServ.userInfo.subscribe((res: UserType) => {
       this.userInfo = res;
     });
+    if (this.userInfo == null || this.userInfo == undefined) {
+      var u = this.vServ.getValue("userProp");
+      this.userInfo = JSON.parse(u);
+    }
+    if (
+      this.usrToken == "" ||
+      this.usrToken == undefined ||
+      this.usrToken == null
+    ) {
+      this.usrToken = this.vServ.getToken();
+    }
+    //console.log(this.userInfo);
+    this.stationId = this.userInfo.stationId;
   }
   Onsub() {}
+  ngOnDestroy() {
+    this.subsc.unsubscribe();
+    this.subsc2.unsubscribe();
+  }
 }
