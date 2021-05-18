d93a84059a447653816ceb4990d3570208d8b14a
RamTheeru <theertha808@gmail.com>
2021-05-18 14:47:01 +0530

    3 files modified

--- src/app/loginhome/file-input/file-input.component.ts
+++ src/app/loginhome/file-input/file-input.component.ts
@@ -4,34 +4,53 @@ import {
   OnDestroy,
   OnInit,
   ViewChild
-} from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PdsApiService } from '../../pds-api.service';
+import { SweetService } from '../../sweet.service';
+import { APIResult } from '../../models/apiresult';
 @Component({
-  selector: "file-input",
-  templateUrl: "file-input.component.html"
+  selector: 'file-input',
+  templateUrl: 'file-input.component.html'
 })
 export class FileInputComponent {
-  @ViewChild("labelImport")
+  @ViewChild('labelImport')
   labelImport: ElementRef;
 
   formImport: FormGroup;
   fileToUpload: File = null;
 
-  constructor() {
+  constructor(private api: PdsApiService, private swServ: SweetService) {
     this.formImport = new FormGroup({
-      importFile: new FormControl("", Validators.required)
+      importFile: new FormControl('', Validators.required)
     });
   }
 
   onFileChange(files: FileList) {
     this.labelImport.nativeElement.innerText = Array.from(files)
       .map(f => f.name)
-      .join(", ");
+      .join(', ');
     this.fileToUpload = files.item(0);
   }
 
   import(): void {
-    console.log("import " + this.fileToUpload.name);
+    console.log('import ' + this.fileToUpload.name);
+    this.api.uploadAttendanceFile(this.fileToUpload).subscribe(
+      (data: APIResult) => {
+        //console.log(data);
+        let status: Boolean = data.status;
+        let m: string = data.message;
+        if (status) {
+          //this.stations = data.stations;
+          this.swServ.showSuccessMessage('Success!!', m);
+        } else {
+          this.swServ.showErrorMessage('Error!!', m);
+        }
+      },
+      err => {
+        //console.log(err.message);
+        this.swServ.showErrorMessage('Network Error!!!', err.message);
+      }
+    );
   }
 }
